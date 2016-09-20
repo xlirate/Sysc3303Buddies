ff077b63b79dad815cf6e0cb021e2314e481e3c3
griffinszymanskibarr <griffinszymanskibarr@AA0508-04.labs.sce.carleton.ca>
2016-09-20 12:14:14 -0400

    Head for the intor to git lab

---
+++ src/buddies/AddressBook.java
@@ -0,0 +1,25 @@
+package buddies;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class AddressBook {
+	private Set<BuddyInfo> Buddies;
+	
+	public AddressBook(Collection<BuddyInfo> c){
+		Buddies = new HashSet<BuddyInfo>(c);
+	}
+	public AddressBook(){
+		this(Collections.emptyList());
+	}
+	
+	public boolean addBuddy(BuddyInfo b){
+		return Buddies.add(b);
+	}
+	
+	public boolean removeBuddy(BuddyInfo b){
+		return Buddies.remove(b);
+	}
+}
---
+++ src/buddies/BuddyInfo.java
@@ -0,0 +1,71 @@
+package buddies;
+
+import java.util.Objects;
+
+public class BuddyInfo {
+	private String name;
+	private String address;
+	private String phoneNumber;//phone numbers can be complicated. They can have +s and leading 0s
+	
+	public BuddyInfo(String name, String address, String phoneNumber){
+		setName(name);
+		setAddress(address);
+		setPhoneNumber(phoneNumber);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	@Override
+	public boolean equals(Object o){
+		if(o == null){
+			return false;
+		}
+		
+		if(o.getClass().equals(this.getClass())){
+			BuddyInfo b = (BuddyInfo)o;
+			return Objects.equals(this.getName(), b.getName()) && 
+					Objects.equals(this.getAddress(), b.getAddress()) && 
+					Objects.equals(this.getPhoneNumber(), b.getPhoneNumber());
+		}else{
+			return false;
+		}
+	}
+	
+	@Override
+	public int hashCode(){
+		int h = 0;
+		if(this.getName()!=null){
+			h += this.getName().hashCode();
+		}
+		if(this.getAddress()!=null){
+			h += this.getAddress().hashCode();
+		}
+		if(this.getPhoneNumber()!=null){
+			h += this.getPhoneNumber().hashCode();
+		}
+		return h;
+	}
+	
+}
